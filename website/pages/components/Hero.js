7fa76d23da7e69bd3d32c6c01a00b5e04e0d7db9
kooya3 <elyeestatua@gmail.com>
2022-11-12 08:49:17 +0300

    changed the original font to Roboto mono and added title tags to the hero section

--- website/pages/components/Hero.js
+++ website/pages/components/Hero.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div>
+      {/* Title */}
+      <div className="flex flex-col items-center pt-3  md:pt-5 lg:pt-10 text-[25px] sm:text-[25px] lg:text-[38px]">
+        <p className="font-semibold">Elyees Tatua </p>
+        <p className="font-semibold">Web Developer + UI/UX Interaction Designer</p>
+        <p className="font-semibold"> Based in Kenya 🇰🇪</p>
+</div>
+      {/* Main */}
+      {/* Left */}
+      {/* Middle */}
+      {/* Right */}
+    </div>
   )
 }
 
