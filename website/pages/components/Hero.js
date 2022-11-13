aa5f576abd55ac3c4a3612d0d8294d2044959e49
kooya3 <elyeestatua@gmail.com>
2022-11-13 15:27:47 +0300

    added the right section of the page and started designing the page for small screens

--- website/pages/components/Hero.js
+++ website/pages/components/Hero.js
@@ -13,21 +13,28 @@ function Hero() {
         <p className="font-semibold"> Based in Kenya 🇰🇪</p>
 </div>
 </div>
-<div className="flex items-center justify-center pt-8">
+<div className="flex items-center justify-center pt-8 sm:justify-between">
         {/* Main */}
       {/* Left */}
-      <div className="hidden sm:flex w-[10rem]">
+      <div className="hidden sm:flex w-[10.5rem]">
         <div className="">
-          <p>Biography </p>  
+          <p className="text-gray-400 text-[14px] pb-3">Biography </p>  
           <p>Work for money and design for love!I'm Elyees, an UI/UX Interaction Designer based in Kenya</p>  
           <p>I have been coding for 4+ years</p>   
         </div>              
       </div>
       {/* Middle */}
-      <div className="transform-gpu w-50 h-[29rem] bg-gray-300 overflow-hidden -mt-4 rounded-full flex">
-        <Image src={Picture } className="object-cover pt-14 " />
+      <div className="transform-gpu w-50 h-[27rem] bg-gray-300 overflow-hidden -mt-4 rounded-full flex sm:mt-4 mx-4">
+        <Image src={Picture } className="object-cover pt-16 " />
       </div>
       {/* Right */}
+      <div className="hidden sm:flex w-[10.5rem] text-right">
+        <div className="">
+          <p className="text-gray-400 text-[14px] pb-3">Biography </p>  
+          <p>Work for money and design for love!I'm Elyees, an UI/UX Interaction Designer based in Kenya</p>  
+          <p>I have been coding for 4+ years</p>   
+        </div>              
+      </div>
 </div>
     </div>
   )
