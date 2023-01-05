abcb4d01e798844c7aeccc21ae84df79e01e0f69
kooya3 <elyeestatua@gmail.com>
2023-01-05 12:29:01 +0300

    fixed the hero section compilation error

--- website/pages/index.js
+++ website/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from './components/Navbar.js'
-import Hero from './components/Hero'
+import Hero from './assets/components/Hero'
 
 export default function Home() {
   return (
